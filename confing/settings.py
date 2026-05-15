aef8cc522881e7c782f260611cb1bd4dfa2e2a26
Yasser Illanes <yass.illanes@duocuc.cl>
2026-05-14 20:26:14 -0400

    feat: agregar clases de permisos predeterminadas para el framework REST

--- confing/settings.py
+++ confing/settings.py
@@ -135,6 +135,9 @@ from datetime import timedelta
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
